eb089b8dac901dd2541b84f6ab9cfbb17c0e0361
zwei2stein <zwei2stein@gmail.com>
2013-01-25 08:40:46 +0000

    Added
    siege detection
    workshop completition
    skill rust/gains
    
    very rusty state is not detected properly yet

--- SoundSense/dfhack/scripts/soundsense.lua
+++ SoundSense/dfhack/scripts/soundsense.lua
@@ -1,3 +1,5 @@
+-- Log aditional info about events in fortress for use in soundsense or other tools.
+
 log = io.open("ss_fix.log", "a")
 
 function msg(m, c)
@@ -15,13 +17,13 @@ end
 local season = math.floor(df.global.cur_year_tick/100800) 
 
 if season == 0 then
-	msg("Spring has arrived on the calendar.",COLOR_LIGHTGREEN)
+	msg("Spring has arrived on the calendar.")
 elseif season == 1 then
-	msg("Summer has arrived on the calendar.",COLOR_LIGHTGREEN)
+	msg("Summer has arrived on the calendar.")
 elseif season == 2 then
-	msg("Autumn has arrived on the calendar.",COLOR_LIGHTGREEN)
+	msg("Autumn has arrived on the calendar.")
 elseif season == 3 then
-	msg("Winter has arrived on the calendar.",COLOR_LIGHTGREEN)
+	msg("Winter has arrived on the calendar.")
 end
 
 -- WEATHER FIX
@@ -41,17 +43,23 @@ for x=0, #df.global.current_weather-1 do
 end
 
 if (not snowing and not raining) then
-	msg("The weather has cleared.",COLOR_LIGHTGREEN)
+	msg("The weather has cleared.")
 elseif raining then
-	msg("It has started raining.",COLOR_LIGHTGREEN)
+	msg("It has started raining.")
 elseif snowing then
-	msg("A snow storm has come.",COLOR_LIGHTGREEN)
+	msg("A snow storm has come.")
 end
 
--- SIEGE FIX
+-- Periodic checkups
+
+local workshopTypes = { "Carpenters Workshop", "Farmers Workshop", "Masons Workshop", "Craftsdwarfs Workshop", "Jewelers Workshop", "Metalsmiths Forge", "Magma Forge", "Bowyers Workshop", "Mechanics Workshop", "Siege Workshop", "Butchers Workshop", "Leatherworks Workshop",  "Tanners Workshop", "Clothiers Workshop", "Fishery", "Still", "Loom", "Quern", "Kennels", "Kitchen", "Ashery", "Dyers Workshop", "Millstone", "Custom", "Tool" }
+local furnaceTypes = { "Wood Furnace", "Smelter", "Glass Furnace", "Kiln", "Magma Smelter", "Magma Glass Furnace", "Magma Kiln", "Custom Furnace" }
 
 old_expedition_leader = nil
 old_mayor = nil
+siege = false
+buildStates = {}
+unit_skills = {}
 
 first_run = true
 
@@ -60,6 +68,9 @@ local function event_loop()
 
 	local expedition_leader = nil
 	local mayor = nil
+	
+	old_siege = siege
+	siege = false
 	
 	for i=0, #units-1 do
 		local unit = units[i]
@@ -75,7 +86,69 @@ local function event_loop()
 					--print(dfhack.TranslateName(dfhack.units.getVisibleName(unit)).." "..positions[p].position.name[0])
 				end
 			end
+			
+			if unit_skills[unit.id] == nil then
+				unit_skills[unit.id] = {}
+			end
+
+			for _, value in pairs(unit.status.current_soul.skills) do 
+			
+				local rating = value.rating
+				if rating > 15 then
+					rating = 15 -- hide legendary+
+				end
+			
+				if unit_skills[unit.id][value.id] == nil then
+					unit_skills[unit.id][value.id] = {}
+					unit_skills[unit.id][value.id].rusty = false
+					unit_skills[unit.id][value.id].very_rusty = false
+					unit_skills[unit.id][value.id].proficient = false
+					unit_skills[unit.id][value.id].accomplished = false
+					unit_skills[unit.id][value.id].legendary = false
+				end
+
+				if rating == 15 and unit_skills[unit.id][value.id].legendary == false and not first_run then
+					unit_skills[unit.id][value.id].legendary = true
+					unit_skills[unit.id][value.id].accomplished = true
+					unit_skills[unit.id][value.id].proficient = true
+					print(dfhack.TranslateName(dfhack.units.getVisibleName(unit)).." became "..df.skill_rating.attrs[rating].caption.." "..df.job_skill.attrs[df.job_skill[value.id]].caption_noun..".")
+				end
+				if rating == 10 and unit_skills[unit.id][value.id].accomplished == false and not first_run then
+					unit_skills[unit.id][value.id].accomplished = true
+					unit_skills[unit.id][value.id].proficient = true
+					print(dfhack.TranslateName(dfhack.units.getVisibleName(unit)).." became "..df.skill_rating.attrs[rating].caption.." "..df.job_skill.attrs[df.job_skill[value.id]].caption_noun..".")
+				end
+				if rating == 5 and unit_skills[unit.id][value.id].proficient == false and not first_run then
+					unit_skills[unit.id][value.id].proficient = true
+					print(dfhack.TranslateName(dfhack.units.getVisibleName(unit)).." became "..df.skill_rating.attrs[rating].caption.." "..df.job_skill.attrs[df.job_skill[value.id]].caption_noun..".")
+				end
+				
+				local rusty = false
+				if value.rating > 0 and value.rating * 0.5 <= value.rusty then
+					rusty = true
+				end
+				
+				if rusty and unit_skills[unit.id][value.id].rusty == false and not first_run then
+					print(dfhack.TranslateName(dfhack.units.getVisibleName(unit)).." became rusty "..df.skill_rating.attrs[rating].caption.." "..df.job_skill.attrs[df.job_skill[value.id]].caption_noun..".".. value.rusty .. ".")
+				end
+				if rusty == false and unit_skills[unit.id][value.id].rusty and not first_runthen
+					print(dfhack.TranslateName(dfhack.units.getVisibleName(unit)).." is no longer rusty "..df.skill_rating.attrs[rating].caption.." "..df.job_skill.attrs[df.job_skill[value.id]].caption_noun..".".. value.rusty .. ".")
+				end
+				unit_skills[unit.id][value.id].rusty = rusty
+				
+			end
+			
 		end
+		-- siege detection
+		if unit.flags1.active_invader then
+			siege = true
+		end
+	end
+	
+	if siege ~= old_siege and siege then
+		msg("A force of vile darkness has arrived!")
+	elseif siege ~= old_siege and not siege then
+		msg("Siege was broken.")
 	end
 
 	if expedition_leader ~= old_expedition_leader then
@@ -104,6 +177,36 @@ local function event_loop()
 			end
 		end
 		old_mayor = mayor
+	end
+	
+	local buildings = df.global.world.buildings.all
+	
+	for i=0, #buildings-1 do
+		local building = buildings[i]
+		if getmetatable(building) == "building_workshopst" then
+			if buildStates[building.id] == nil then
+				buildStates[building.id] = building.flags.exists
+			end
+			local oldval = buildStates[building.id]
+			if oldval ~= building.flags.exists then
+				buildStates[building.id] = building.flags.exists
+				if building.flags.exists then
+					print(workshopTypes[building.type+1].." was built.")
+				end
+			end
+		end
+		if getmetatable(building) == "building_furnacest" then
+			if buildStates[building.id] == nil then
+				buildStates[building.id] = building.flags.exists
+			end
+			local oldval = buildStates[building.id]
+			if oldval ~= building.flags.exists then
+				buildStates[building.id] = building.flags.exists
+				if building.flags.exists then
+					print(furnaceTypes[building.type+1].." was built.")
+				end
+			end
+		end
 	end
 	
 	first_run = false
